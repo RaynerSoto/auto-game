15ff971eb05642f7fed3bb4f4ad1ab25e9795a4a
Rayner Alejandro Soto Martínez <raynersoto01@gmail.com>
2024-02-04 23:47:05 -0500

    Refactorizando el código V1

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
-let titulo = document.querySelector('h1');
-titulo.innerText = 'Autojuego de ordenador';
-let parrafo = document.querySelector('.texto__parrafo');
-parrafo.innerHTML = 'El objetivo es ver cuanto tiempo necesita un ordenador para resolver un número entre 1 y 1000000000000000000000'
+window.addEventListener('load',() =>{
+    asignarNombre('Autojuego de ordenador','h1');
+    asignarNombre('El objetivo es ver cuanto tiempo necesita un ordenador para resolver un número entre 1 y 1000000000000000000000','.texto__parrafo')
+});
 
+function asignarNombre(nombre,elemento){
+    let titulo = document.querySelector(elemento);
+    titulo.innerText = nombre;
+}
+
+let boton = document.querySelector('#boton');
+boton.addEventListener('click',() => {
+    game()
+})
 
 function game(){
     let inicializador = 1,adivino,minimo = 1,maximo = 1000000000000000000000;
-    let number = Math.floor(Math.random() * (maximo - minimo) + minimo);
+    let number = generadorNumero(minimo,maximo)
     let fecha = performance.now();
     for (adivino = 0; adivino!= number; inicializador++){
-        adivino = Math.floor(Math.random() * (maximo - minimo) + minimo);
+        adivino = generadorNumero(minimo,maximo);
         if (adivino != number)
             number < adivino ? maximo = adivino : minimo = adivino;
     }
-    let label = document.getElementById('label');
+
     fecha_fin = performance.now();
-    label.innerHTML = `Tu ordenador se tardó ${fecha_fin - fecha} milisegundos y tuvo que hacer ${inicializador} iteraciones , el nùmero era ${adivino} y se generó ${number}`;
+    asignarNombre(`Tu ordenador se tardó ${fecha_fin - fecha} milisegundos y tuvo que hacer ${inicializador} iteraciones , el nùmero era ${adivino} y se generó ${number}`,'label')
+}
+
+function generadorNumero(minimo,maximo){
+    return Math.floor(Math.random() * (maximo - minimo) + minimo);
 }
